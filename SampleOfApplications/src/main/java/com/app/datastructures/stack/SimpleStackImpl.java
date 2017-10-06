ad885b69615bdb23762bfc4faa6708825132060a
cyber-kid <amyrgorod@gmail.com>
2017-10-06 21:31:03 +0300

    add unit tests

---
+++ SampleOfApplications/src/main/java/com/app/datastructures/stack/SimpleStackImpl.java
@@ -0,0 +1,23 @@
+package com.app.datastructures.stack;
+
+public class SimpleStackImpl implements SimpleStack {
+    @Override
+    public boolean isEmpty() {
+        return false;
+    }
+
+    @Override
+    public int size() {
+        return 0;
+    }
+
+    @Override
+    public void push(Object object) {
+
+    }
+
+    @Override
+    public Object pop() {
+        return null;
+    }
+}
