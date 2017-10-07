26afcd2d3d9344863e328ebb4fea8d0fabddb810
cyber-kid <amyrgorod@gmail.com>
2017-10-07 15:13:19 +0300

    add unit tests for queue

---
+++ SampleOfApplications/src/main/java/com/app/datastructures/queue/SimpleQueueImpl.java
@@ -0,0 +1,23 @@
+package com.app.datastructures.queue;
+
+public class SimpleQueueImpl implements SimpleQueue{
+    @Override
+    public int size() {
+        return 0;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return false;
+    }
+
+    @Override
+    public void enqueue(Object object) {
+
+    }
+
+    @Override
+    public Object dequeue() {
+        return null;
+    }
+}
